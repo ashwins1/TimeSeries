c4664ee6ca9c864ef560f16486d779ac387c29a9
Ashwin Rajan Siripurapu <ashwins1@corn03.stanford.edu>
2014-04-20 11:15:41 -0700

    Fixed type errors in Correlation.hs

--- Correlation.hs
+++ Correlation.hs
@@ -4,14 +4,19 @@ module TimeSeries.Correlation
 , pacf
 ) where
 
+import TimeSeries.TimeSeries ( TimeSeries (..) )
+
 mean :: [Double] -> Double
-mean xs = (sum xs) / (length xs)
+mean xs = (sum xs) / (fromIntegral $ length xs)
 
 autocov :: Int -> TimeSeries Double -> Double
 autocov h xs = (1/n) * (sum $ zipWith (*) xcnt (drop h xcnt))
-  where n    = length ts :: Double
+  where n    = fromIntegral $ length xs :: Double
         xbar = mean xs
         xcnt = [x - xbar | x <- xs]
 
 acf :: Int -> TimeSeries Double -> Double
 acf h xs = (autocov h xs) / (autocov 0 xs)
+
+pacf :: Int -> TimeSeries Double -> Double
+pacf = undefined
